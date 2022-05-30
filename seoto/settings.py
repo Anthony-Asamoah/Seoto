9756282508b1ce08defe5b9a1618ee069c5674bb
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2022-05-30 21:01:43 +0000

    updated alowed hosts list to suit pythonanywhere

--- seoto/settings.py
+++ seoto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'seoto.pythonanywhere.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'tony48.pythonanywhere.com']
 
 # Application definition
 
