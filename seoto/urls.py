ff304209ed86a1db48a44f3e3dd9941e58a26ec2
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2022-06-05 23:03:06 +0000

    included jotter urls to django project

--- seoto/urls.py
+++ seoto/urls.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from home.views import index
+
 from foodie.views import foodie
 from interest_calc.views import interest
 from rhymes.views import rhymes
 from throw_a_die.views import die
 from flip_a_coin.views import coin
-
-from home.views import index
 
 
 urlpatterns = [
@@ -23,5 +23,7 @@ urlpatterns = [
     path('interest_calculator', interest, name='interest_calculator'),
     path('rhymes', rhymes, name='rhymes'),
     path('throw_a_die', die, name='throw_a_die'),
-    path('flip_a_coin', coin, name='flip_a_coin')
+    path('flip_a_coin', coin, name='flip_a_coin'),
+    path('jotter', include('jotter.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
