1a9f3bcebcfbd41ad46117d34b3d1b13794dc315
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2022-06-11 13:19:00 +0000

    Debug Off; for publishing

--- seoto/settings.py
+++ seoto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'tony48.pythonanywhere.com']
 
