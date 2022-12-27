8e8c83bee8388be135b4717955abda661ad6e1ea
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2022-12-27 21:21:29 +0000

    included exception handling for prod and dev environments

--- seoto/utils.py
+++ seoto/utils.py
@@ -2,8 +2,12 @@ from os import environ
 
 
 def load_variables_into_environment():
-	with open(r".env", 'r') as env:
-		variables = env.readlines()
+	try:
+		with open(r".env", 'r') as env:
+			variables = env.readlines()
+	except FileNotFoundError:
+		with open(r"../.env", 'r') as env:
+			variables = env.readlines()
 
 	for variable in variables:
 		environ[variable.split('=')[0]] = str(variable.split('=')[1].replace('\n', ''))
