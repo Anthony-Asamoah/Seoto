dea61bfab2db39b9b477ed08838dbf5491ce088d
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2023-12-27 16:28:02 +0000

    add method to send mail when message is saved

--- author/models.py
+++ author/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db import models
 from django.forms import ModelForm, TextInput
 from django.utils import timezone
@@ -29,6 +31,13 @@ class message(models.Model):
 
 	def __str__(self):
 		return f'Message from {self.name}'
+
+	def forward_to_email(self):
+		mail_title = f'New message on Seoto from {self.name}'
+		mail_body = f'Sender: {self.name}\nSubject: {self.subject}\n\n{self.message}\n\nSubmitted on: {self.submitted_on}\nemail: {self.email}'
+		mail_sender = settings.EMAIL_HOST_USER
+		mail_recipient = [settings.EMAIL_HOST_USER]
+		send_mail(mail_title, mail_body, mail_sender, mail_recipient, fail_silently=False)
 
 
 class contact_form(ModelForm):
--- author/views.py
+++ author/views.py
@@ -1,32 +1,34 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.shortcuts import render, redirect
+
 from .models import contact_form, stack
 
 
 def about(request):
-	user = request.user
-	if user.is_authenticated:
-		form = contact_form(initial={
-			'name': f'{user.first_name.title()} {user.last_name.title()}',
-			'email': user.email
-		})
-	else:
-		form = contact_form()
+    user = request.user
+    if user.is_authenticated:
+        form = contact_form(initial={
+            'name': f'{user.first_name.title()} {user.last_name.title()}',
+            'email': user.email
+        })
+    else:
+        form = contact_form()
 
-	if request.method == 'POST':
-		form = contact_form(request.POST)
-		if form.is_valid():
-			form.save()
-			messages.success(request, 'Message Submitted.')
-			return redirect('about')
-		else:
-			messages.error(request, 'Could not be submitted.')
-	try:
-		stk = stack.objects.filter(is_active=True).first()
-	except Exception as e:
-		stk = None
-	context = {
-		'form': form,
-		'stack': stk
-	}
-	return render(request, 'author/about.html', context)
+    if request.method == 'POST':
+        form = contact_form(request.POST)
+        if form.is_valid():
+            details = form.save()
+            details.forward_to_email()
+            messages.success(request, 'Message Submitted.')
+            return redirect('about')
+        else:
+            messages.error(request, 'Could not be submitted.')
+    try:
+        stk = stack.objects.filter(is_active=True).first()
+    except Exception as e:
+        stk = None
+    context = {
+        'form': form,
+        'stack': stk
+    }
+    return render(request, 'author/about.html', context)
