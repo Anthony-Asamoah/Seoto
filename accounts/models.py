669ba521a2b080dd2d1a0a9b12192d536456b289
Anthony Asamoah <anthonyasamoah48@gmail.com>
2024-06-08 13:25:26 +0000

    refine file path for profile picture

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,18 @@
-from django.db import models
+from functools import partial
+
 from django.contrib.auth.models import User
+from django.db import models
+
+from accounts.utils import user_directory_file_path
 
 
 class user_profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	contact = models.CharField(max_length=15, null=True, blank=True)
-	picture = models.ImageField(upload_to=f'accounts/%Y-%m-%d', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=15, null=True, blank=True)
+    picture = models.ImageField(
+        upload_to=partial(user_directory_file_path, prefix='accounts/%Y-%m-%d'),
+        blank=True, null=True
+    )
 
-	def __str__(self):
-		return f'{self.user.username}'
+    def __str__(self):
+        return f'{self.user.username}'
