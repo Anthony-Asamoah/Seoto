0fe335daf5c928a4f45f75cafe1b58d1af071cb3
QuanChii <anthonyasamoah48@gmail.com>
2025-10-19 15:32:51 +0000

    refine login form: improve design, add custom styles, animations, and custom authentication form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,22 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control password-input',
+            'id': 'id_password'
+        })
+        self.fields['username'].widget.attrs.update({
+            'id': 'id_username'
+        })
 
 
 class RegisterForm(UserCreationForm):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views
 from .views import register, profile
+from .forms import LoginForm
 
 
 urlpatterns = [
-	path("login/", views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+	path("login/", views.LoginView.as_view(template_name='accounts/login.html', authentication_form=LoginForm), name='login'),
 	path("logout/", views.LogoutView.as_view(), name="logout"),
 	path(
 		"password_change/", views.PasswordChangeView.as_view(), name="password_change"
