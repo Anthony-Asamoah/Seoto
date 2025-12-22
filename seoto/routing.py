6f879c29471d67cb6e63ee03926c2fdb48fc8d2a
QuanChii <anthonyasamoah48@gmail.com>
2025-12-22 11:07:06 +0000

    add meal orders in foodie app

---
+++ seoto/routing.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from foodie.routing import websocket_urlpatterns as foodie_ws
+
+# Project-level WebSocket URL patterns
+websocket_urlpatterns = [
+    # Namespace foodie websocket endpoints
+    *foodie_ws,
+]
