8799e284f9f21e42a9cc85aea2752295cf7b47bd
QuanChii <anthonyasamoah48@gmail.com>
2026-08-03 23:15:11 +0000

    Disable query string authentication for S3 static files configuration

--- src/infrastructure/core/settings.py
+++ src/infrastructure/core/settings.py
@@ -235,6 +235,7 @@ if MEDIA_STORAGE == 'AWS':
             'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
             'OPTIONS': {
                 'location': f'{AWS_S3_BUCKET_PREFIX}/staticfiles',
+                'querystring_auth': False,
             },
         },
     }
